c9013d2baa51a8b7f2713d5f027845608bd59496
Alinur Rinatov <rinatsadyrbaev888@gmail.com>
2026-08-01 16:57:53 +0600

    hate codfroces more and more

--- 131a.cpp
+++ 131a.cpp
@@ -11,7 +11,31 @@ using pii = pair<ll, ll>;
 #define sz(x) (int)(x).size()
 
 void solve() {  
-    
+    string s; cin >> s;
+    if (sz(s)==1){
+        if (islower(s[0])) cout << (char)toupper(s[0]);
+        else cout << (char)tolower(s[0]);
+        return;
+    }
+    bool o = true;
+    for (int  i=1; i<sz(s); ++i){
+        char c = s[i];
+        if ( c != (char)toupper(c) ){
+            o=false;
+        }
+    }
+    if (o){
+        if (s[0] == (char)tolower(s[0])){
+            cout << (char)toupper(s[0]);
+        }else{
+            cout << (char)tolower(s[0]);
+        }
+        for (int i=1; i<sz(s); ++i){
+            cout <<(char)tolower(s[i]);
+        }
+    }else{
+        cout << s;
+    }
 }
 
 int main() {
@@ -23,7 +47,7 @@ int main() {
     cin.tie(0);
 
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t--) {
         solve();
     }

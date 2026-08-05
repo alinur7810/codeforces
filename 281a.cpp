9d4efa282463973127e86989c1678619a7457d43
Alinur Rinatov <rinatsadyrbaev888@gmail.com>
2026-08-05 20:42:26 +0600

    sup my str

---
+++ 281a.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<ll, ll>;
+
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define sz(x) (int)(x).size()
+
+void solve() {  
+    string s; cin >> s;
+    s[0] = (isupper(s[0]) ? s[0] : (char)toupper(s[0])); 
+    cout << s;
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin); 
+    #endif
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+}
